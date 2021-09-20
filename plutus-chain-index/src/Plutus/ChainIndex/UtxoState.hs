e08c56cbef37fe713b48adfc4c9a038a69dc3956
Konstantinos Lambrou-Latreille <konstantinos.lambrou@iohk.io>
2021-09-20 10:31:00 -0400

    SCP-2734: API for watching the state of tx outputs.
    * Added awaitTxOutStatusChange API call in plutus-contract and updated the emulator to respond to those calls.
    * Added waitTxOutStatusChange API call in plutus-pab, updated the request handler to respond to those calls.
    * Created type `TxOutBalance` which keeps track of spent and unspent transaction outptus of a set of transactions.
    * Added types for representing the transaction output status. Refactored the type of the old transaction status as well.
    * Refactored testing module in plutus-chain-index.
    * TxUtxoBalance in now in it's own module in plutus-chain-index.
    * The testing in plutus-pab is much faster because the time per slot is now 1ms instead of 1s.

--- plutus-chain-index/src/Plutus/ChainIndex/UtxoState.hs
+++ plutus-chain-index/src/Plutus/ChainIndex/UtxoState.hs
@@ -15,18 +15,11 @@ module Plutus.ChainIndex.UtxoState(
     UtxoState(..)
     , usTxUtxoData
     , usTip
-    , TxUtxoBalance(..)
-    , tubUnspentOutputs
-    , tubUnmatchedSpentInputs
     , UtxoIndex
     , utxoState
     , utxoBlockCount
-    , fromBlock
-    , fromTx
-    , isUnspentOutput
     , tip
     , viewTip
-    , unspentOutputs
     -- * Extending the UTXO index
     , InsertUtxoPosition(..)
     , InsertUtxoSuccess(..)
@@ -35,14 +28,12 @@ module Plutus.ChainIndex.UtxoState(
     -- * Rollbacks
     , RollbackFailed(..)
     , RollbackResult(..)
-    , rollback
     , rollbackWith
     -- * Limit the UTXO index size
     , ReduceBlockCountResult(..)
     , reduceBlockCount
     ) where
 
-import           Codec.Serialise                   (Serialise)
 import           Control.Lens                      (makeLenses, view)
 import           Data.Aeson                        (FromJSON, ToJSON)
 import           Data.FingerTree                   (FingerTree, Measured (..))
@@ -50,44 +41,17 @@ import qualified Data.FingerTree                   as FT
 import           Data.Function                     (on)
 import           Data.Monoid                       (Sum (..))
 import           Data.Semigroup.Generic            (GenericSemigroupMonoid (..))
-import           Data.Set                          (Set)
-import qualified Data.Set                          as Set
 import           GHC.Generics                      (Generic)
-import           Ledger                            (TxIn (txInRef), TxOutRef (..))
 import           Plutus.ChainIndex.ChainIndexError (InsertUtxoFailed (..), RollbackFailed (..))
 import           Plutus.ChainIndex.ChainIndexLog   (InsertUtxoPosition (..))
-import           Plutus.ChainIndex.Tx              (ChainIndexTx (..), citxInputs, txOutsWithRef)
-import           Plutus.ChainIndex.Types           (Point (..), Tip (..), pointsToTip)
+import           Plutus.ChainIndex.Types           (Point (Point, PointAtGenesis), Tip (..), pointsToTip)
 import           Prettyprinter                     (Pretty (..))
-
--- | The effect of a transaction (or a number of them) on the utxo set.
-data TxUtxoBalance =
-    TxUtxoBalance
-        { _tubUnspentOutputs       :: Set TxOutRef -- ^ Outputs newly added by the transaction(s)
-        , _tubUnmatchedSpentInputs :: Set TxOutRef -- ^ Outputs spent by the transaction(s)
-        }
-        deriving stock (Eq, Show, Generic)
-        deriving anyclass (FromJSON, ToJSON, Serialise)
-
-
-makeLenses ''TxUtxoBalance
-
-instance Semigroup TxUtxoBalance where
-    l <> r =
-        TxUtxoBalance
-            { _tubUnspentOutputs       = _tubUnspentOutputs r <> (_tubUnspentOutputs l `Set.difference` _tubUnmatchedSpentInputs r)
-            , _tubUnmatchedSpentInputs = (_tubUnmatchedSpentInputs r `Set.difference` _tubUnspentOutputs l) <> _tubUnmatchedSpentInputs l
-            }
-
-instance Monoid TxUtxoBalance where
-    mappend = (<>)
-    mempty = TxUtxoBalance mempty mempty
 
 -- | UTXO / ledger state, kept in memory. We are only interested in the UTXO set, everything else is stored
 --   on disk. This is OK because we don't need to validate transactions when they come in.
 data UtxoState a =
     UtxoState
-        { _usTxUtxoData :: a -- One of 'TxUtxoBalance' or 'TxIdState'
+        { _usTxUtxoData :: a -- One of 'TxUtxoBalance', 'TxOutBalance' or 'TxIdState'
         , _usTip        :: Tip -- ^ Tip of our chain sync client
         }
         deriving stock (Eq, Show, Generic)
@@ -96,15 +60,8 @@ data UtxoState a =
 
 makeLenses ''UtxoState
 
-fromTx :: ChainIndexTx -> TxUtxoBalance
-fromTx tx =
-    TxUtxoBalance
-        { _tubUnspentOutputs = Set.fromList $ fmap snd $ txOutsWithRef tx
-        , _tubUnmatchedSpentInputs = Set.mapMonotonic txInRef (view citxInputs tx)
-        }
-
-newtype BlockCount = BlockCount { getBlockCount :: Int } deriving (Semigroup, Monoid) via (Sum Int)
-
+newtype BlockCount = BlockCount { getBlockCount :: Int }
+  deriving (Semigroup, Monoid) via (Sum Int)
 type UtxoIndex a = FingerTree (BlockCount, UtxoState a) (UtxoState a)
 instance Monoid a => Measured (BlockCount, UtxoState a) (UtxoState a) where
     measure u = (BlockCount 1, u)
@@ -115,10 +72,6 @@ utxoState = snd . measure
 utxoBlockCount :: Monoid a => UtxoIndex a -> Int
 utxoBlockCount = getBlockCount . fst . measure
 
--- | Whether a 'TxOutRef' is a member of the UTXO set (ie. unspent)
-isUnspentOutput :: TxOutRef -> UtxoState TxUtxoBalance -> Bool
-isUnspentOutput r = Set.member r . view (usTxUtxoData . tubUnspentOutputs)
-
 tip :: UtxoState a -> Tip
 tip = view usTip
 
@@ -127,18 +80,6 @@ viewTip = tip . utxoState
 
 instance Eq a => Ord (UtxoState a) where
     compare = compare `on` tip
-
--- | The UTXO set
-unspentOutputs :: UtxoState TxUtxoBalance -> Set TxOutRef
-unspentOutputs = view (usTxUtxoData . tubUnspentOutputs)
-
--- | 'UtxoIndex' for a single block
-fromBlock :: Tip -> [ChainIndexTx] -> UtxoState TxUtxoBalance
-fromBlock tip_ transactions =
-    UtxoState
-            { _usTxUtxoData = foldMap fromTx transactions
-            , _usTip        = tip_
-            }
 
 data InsertUtxoSuccess a =
     InsertUtxoSuccess
@@ -172,11 +113,25 @@ data RollbackResult a =
         , rolledBackIndex :: UtxoIndex a
         }
 
--- | Perform a rollback on the utxo index
-rollback :: Point
-         -> UtxoIndex TxUtxoBalance
-         -> Either RollbackFailed (RollbackResult TxUtxoBalance)
-rollback = rollbackWith const
+data ReduceBlockCountResult a
+    = BlockCountNotReduced
+    | ReduceBlockCountResult
+        { reducedIndex  :: UtxoIndex a
+        , combinedState :: UtxoState a
+        }
+
+-- | Reduce the number of 'UtxoState's. The given number is the minimum, the index is reduced when it larger than twice that size.
+-- The new index is prefixed with one 'UtxoState' that contains the combined state of the removed 'UtxoState's.
+reduceBlockCount :: Monoid a => Int -> UtxoIndex a -> ReduceBlockCountResult a
+reduceBlockCount minCount ix
+    | utxoBlockCount ix <= 2 * minCount = BlockCountNotReduced
+    | otherwise =
+        let (old, keep) = FT.split ((> (utxoBlockCount ix - minCount)) . getBlockCount . fst) ix
+            combinedState = utxoState old
+        in ReduceBlockCountResult
+            { reducedIndex = combinedState FT.<| keep
+            , combinedState = combinedState
+            }
 
 -- | Perform a rollback on the utxo index, with a callback to calculate the new index.
 rollbackWith
@@ -208,23 +163,3 @@ rollbackWith f targetPoint idx@(viewTip -> currentTip)
       pointLessThanTip PointAtGenesis  _               = True
       pointLessThanTip (Point pSlot _) (Tip tSlot _ _) = pSlot < tSlot
       pointLessThanTip _               TipAtGenesis    = False
-
-data ReduceBlockCountResult a
-    = BlockCountNotReduced
-    | ReduceBlockCountResult
-        { reducedIndex  :: UtxoIndex a
-        , combinedState :: UtxoState a
-        }
-
--- | Reduce the number of 'UtxoState's. The given number is the minimum, the index is reduced when it larger than twice that size.
--- The new index is prefixed with one 'UtxoState' that contains the combined state of the removed 'UtxoState's.
-reduceBlockCount :: Monoid a => Int -> UtxoIndex a -> ReduceBlockCountResult a
-reduceBlockCount minCount ix
-    | utxoBlockCount ix <= 2 * minCount = BlockCountNotReduced
-    | otherwise =
-        let (old, keep) = FT.split ((> (utxoBlockCount ix - minCount)) . getBlockCount . fst) ix
-            combinedState = utxoState old
-        in ReduceBlockCountResult
-            { reducedIndex = combinedState FT.<| keep
-            , combinedState = combinedState
-            }
