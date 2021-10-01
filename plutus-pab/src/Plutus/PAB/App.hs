a03debba6c68afc622fa1c7b2c8a3168f7e2bf92
silky <noonsilk+-noonsilk@gmail.com>
2021-10-01 15:33:03 +0100

    Shuffling

--- plutus-pab/src/Plutus/PAB/App.hs
+++ plutus-pab/src/Plutus/PAB/App.hs
@@ -63,9 +63,9 @@ import           Plutus.PAB.Core.ContractInstance.STM           as Instances
 import qualified Plutus.PAB.Db.Beam.ContractStore               as BeamEff
 import           Plutus.PAB.Db.Memory.ContractStore             (InMemInstances, initialInMemInstances)
 import qualified Plutus.PAB.Db.Memory.ContractStore             as InMem
+import           Plutus.PAB.Db.Schema                           (checkedSqliteDb)
 import           Plutus.PAB.Effects.Contract                    (ContractDefinition (..))
 import           Plutus.PAB.Effects.Contract.Builtin            (Builtin, BuiltinHandler (..), HasDefinitions (..))
-import           Plutus.PAB.Effects.DbStore                     (checkedSqliteDb)
 import           Plutus.PAB.Monitoring.Monitoring               (convertLog, handleLogMsgTrace)
 import           Plutus.PAB.Monitoring.PABLogMsg                (PABLogMsg (..),
                                                                  PABMultiAgentMsg (BeamLogItem, UserLog))
